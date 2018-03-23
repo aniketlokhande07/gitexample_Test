0766e0f7affbe1e8da107405ef98d51d6004af55
aniketlokhande07 <aniketlokhande07@gmail.com>
2018-03-23 16:47:21 +0530

    added new sampleobjec.swift file

---
+++ gitexample/sampleobjec.swift
@@ -0,0 +1,14 @@
+//
+//  sampleobjec.swift
+//  gitexample
+//
+//  Created by saiusers on 23/03/18.
+//  Copyright © 2018 saiusers. All rights reserved.
+//
+
+import Foundation
+class sampleobjec : NSObject {
+    
+    var id = ""
+    var name = ""
+}
