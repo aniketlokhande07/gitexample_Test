50e0bb555353d917737a03b2d4519d46977de432
aniketlokhande07 <aniketlokhande07@gmail.com>
2018-03-23 16:10:54 +0530

    added one table to story board and append array content to tableview

--- gitexample/ViewController.swift
+++ gitexample/ViewController.swift
@@ -8,21 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+  
+    
+    
+    
 
+    @IBOutlet var tblload: UITableView!
     @IBOutlet var lbltitle: UILabel!
+    var arr = ["Support","Followup","FAQ","Account","singout"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        tblload.dataSource = self
+        tblload.delegate = self
         
         var hello = "welcome"
            print(hello)
     }
-
+ 
+    
     override func viewWillAppear(_ animated: Bool) {
         lbltitle.text  =  "Welcome to new project"
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = arr[indexPath.row]
+        return cell!
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
